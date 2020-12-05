2b9c7677ba87cb8b67fa047fe13687db40d014de
Aleksei Guliaev <guliaevaleksei@gmail.com>
2020-12-05 13:59:32 +0600

    Add buttons to choose category

--- tg_bot/main.py
+++ tg_bot/main.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 import firebase_admin
 from firebase_admin import firestore
 from firebase_admin import credentials
@@ -13,6 +14,8 @@ logger = telebot.logger
 telebot.logger.setLevel(logging.DEBUG)  # Outputs debug messages to console.
 
 bot = telebot.TeleBot(os.getenv("JOURNAL_BOT_TOKEN"), parse_mode=None)
+categories = ['Org', 'Tech', 'Other']
+question_dict = {}
 
 
 @bot.message_handler(commands=['start'])
@@ -25,17 +28,47 @@ def start_handler(message):
             f"You need to register. Your id is: {message.from_user.id}")
 
 
+class Question:
+    def __init__(self, text=None, category=None):
+        self.text = text
+        self.category = category
+
+
+@bot.message_handler(commands=['ask_question'])
+def ask_question(message):
+    if is_registered(message.from_user.id):
+        markup = types.ReplyKeyboardMarkup(row_width=3, one_time_keyboard=True)
+        buttons = [types.KeyboardButton(cat) for cat in categories]
+        markup.add(*buttons)
+        bot.send_message(message.chat.id,
+                         "Choose question category",
+                         reply_markup=markup)
+        bot.register_next_step_handler(message, process_category_step)
+    else:
+        bot.reply_to(message, "You need to register first")
+
+
+def process_category_step(message):
+    try:
+        category = message.text
+        question_dict['category'] = category
+        markup = types.ForceReply(selective=False)
+        msg = bot.reply_to(message, 'Your question:', reply_markup=markup)
+        bot.register_next_step_handler(msg, process_question_step)
+    except Exception as e:
+        bot.reply_to(message, 'Something went wrong')
+
+
+def process_question_step(message):
+    try:
+        bot.reply_to(message, commit_task(message))
+    except Exception as e:
+        bot.reply_to(message, 'Something went wrong')
+
+
 @bot.message_handler(commands=['get_id'])
 def get_id_handler(message):
     bot.send_message(message.chat.id, message.from_user.id)
-
-
-@bot.message_handler()
-def question_handler(message):
-    if is_registered(message.from_user.id):
-        bot.reply_to(message, commit_task(message))
-    else:
-        bot.reply_to(message, "You need to register first")
 
 
 def is_registered(user_id):
@@ -79,10 +112,13 @@ def create_task(message, task_id):
 
 
 def create_question(message, tasks_ref, task_id):
-    question = {'title': message.text,
-                'type': 'input',
-                'chat_id': message.from_user.id,
-                'required': True}
+    question = {
+        'title': message.text,
+        'type': 'input',
+        'chat_id': message.from_user.id,
+        'question_category': question_dict.get('category'),
+        'required': True
+    }
     questions_ref = tasks_ref.document(task_id).collection('questions')
     question_id = questions_ref.document().id
     question_ref = questions_ref.document(question_id)
