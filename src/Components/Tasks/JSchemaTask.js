bd3a17bd72fcb3427dd19df8d03606b5653d8663
aTNa <atay.narynov@gmail.com>
2021-04-05 12:49:27 +0600

    Change to bootstrap, add word-break

--- src/Components/Tasks/JSchemaTask.js
+++ src/Components/Tasks/JSchemaTask.js
@@ -20,7 +20,7 @@ import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-import JSchemaForm from "@rjsf/fluent-ui";
+import JSchemaForm from "@rjsf/bootstrap-4";
 import { cloneDeep, isEqual } from 'lodash'
 import ReactMarkdown from 'react-markdown'
 
@@ -823,7 +823,7 @@ const JSchemaTask = () => {
 
 	return (
 		currentUser ?
-			<Grid>
+			<Grid container style={{wordBreak: 'break-word'}}>
 				{dialogType === 'send' && <Dialog
 					state={dialogState}
 					handleClose={handleDialogClose}
