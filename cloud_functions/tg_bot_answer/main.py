3acd4c0b4a23ff590877e66fd7d56eed93244ad2
Aleksei Guliaev <guliaevaleksei@gmail.com>
2021-04-07 15:06:53 +0600

    Change trigger for tg_bot_answer to document update

--- cloud_functions/tg_bot_answer/main.py
+++ cloud_functions/tg_bot_answer/main.py
@@ -10,35 +10,39 @@ def send_answer(event, context):
     collection_path = path_parts[0]
     document_path = '/'.join(path_parts[1:])
 
-    response_ref = client.document(
-        f'{collection_path}/{document_path}/responses/answer')
     task_ref = client.collection(collection_path).document(path_parts[1])
-    if task_ref.get().to_dict().get('is_complete') is True:
-        question_ref = task_ref.collection('questions').document(
-            path_parts[-1])
-        if not question_ref.get().exists:
-            question_ref = task_ref\
-                    .collection('questions')\
-                    .document('form_questions')
+    response_ref = client.document(f'{task_ref.path}/responses/answer')
+    doc_ref = client.document(f'{collection_path}/{document_path}')
+    print(f'task_ref: {task_ref.path}\nresponse_ref: {response_ref.path}\ndoc_ref: {doc_ref.path}')
+    if doc_ref.get()\
+              .to_dict()\
+              .get('status') == 'complete':
+        question_ref = task_ref\
+                .collection('questions')\
+                .document('form_questions')
         question_doc = question_ref.get().to_dict()
         if question_doc:
             chat_id = question_doc.get('chat_id')
             # check if it's response for a bot
             if chat_id:
+                print(f'Chat id: {chat_id}')
                 response_doc = response_ref.get().to_dict()
-                message = get_message(question_doc, response_doc)
-                send_text = f'https://journaltgbot.kloop.io'\
-                            f'/bot{os.environ.get("TG_BOT_TOKEN")}'\
-                            f'/sendMessage?chat_id={chat_id}'\
-                            f'&text={message}'
-                response = requests.get(send_text)
-                print('response: ', response)
+                if response_doc:
+                    message = get_message(question_doc, response_doc)
+                    send_text = f'https://journaltgbot.kloop.io'\
+                                f'/bot{os.environ.get("TG_BOT_TOKEN")}'\
+                                f'/sendMessage?chat_id={chat_id}'\
+                                f'&text={message}'
+                    response = requests.get(send_text)
+                    print('response: ', response)
+                else:
+                    print('No response yet')
             else:
                 pass
         else:
-            print('no such question')
+            print('No question')
     else:
-        print('Task is not completed yet')
+        print(f'Task {task_ref.path} is not completed yet')
 
 
 def get_message(question_doc, response_doc):
